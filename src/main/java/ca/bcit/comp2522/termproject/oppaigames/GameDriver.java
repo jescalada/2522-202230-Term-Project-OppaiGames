906d331be37cde2b52f017083c871f2db55d0935
Juan Escalada <juanescalada175@gmail.com>
2022-11-14 14:24:46 -0800

    Improve layout for main page

--- src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameDriver.java
@@ -2,11 +2,16 @@ package ca.bcit.comp2522.termproject.oppaigames;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,8 +21,16 @@ public class GameDriver extends Application {
 
     @Override
     public void start(Stage stage) {
-        HBox root = new HBox();
+        BorderPane root = new BorderPane();
+        HBox center = new HBox();
+        StackPane top = new StackPane();
+
         Scene scene = new Scene(root, 800, 800);
+
+        Text gameTitle = new Text();
+        gameTitle.setText("The Best Clicker Game In The World");
+        gameTitle.setScaleX(3);
+        gameTitle.setScaleY(3);
 
         Button inventoryButton = new Button("_Inventory");
         Button mapButton = new Button("_Map");
@@ -37,14 +50,19 @@ public class GameDriver extends Application {
             showCraft();
         });
 
-        root.getChildren().addAll(inventoryButton, mapButton, shopButton, craftButton);
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(10);
-
+        center.getChildren().addAll(inventoryButton, mapButton, shopButton, craftButton);
+        center.setSpacing(10);
+        center.setAlignment(Pos.CENTER);
         inventoryButton.setMinSize(100, 100);
         mapButton.setMinSize(100, 100);
         shopButton.setMinSize(100, 100);
         craftButton.setMinSize(100, 100);
+
+        top.getChildren().add(gameTitle);
+        top.setPadding(new Insets(20));
+
+        root.setCenter(center);
+        root.setTop(top);
 
         stage.setScene(scene);
         stage.setTitle("OppaiGames: The Awesome JavaFX Clicker Game");
