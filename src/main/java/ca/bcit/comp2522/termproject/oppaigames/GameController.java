8a54a1b0b012518649b759511ae558f4d013f875
Juan Escalada <juanescalada175@gmail.com>
2022-11-15 15:37:28 -0800

    Add loadRecipes function and helper methods

--- src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameController.java
+++ src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameController.java
@@ -51,6 +51,46 @@ public class GameController {
      * @return
      */
     public List<Recipe> loadRecipes() {
+        List<Recipe> recipes = new ArrayList<>();
+        Scanner sc;
+        try {
+            // pass the path to the file as a parameter
+            File file = new File(RECIPE_FILE_LOCATION);
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                // Parse recipes line by line
+                String[] recipeParameters = sc.nextLine().split(" \\| ");
+                String recipeName = recipeParameters[0];
+                String recipeDescription = recipeParameters[1];
+                Map<Item, Integer> ingredients = new HashMap<>();
+                String[] ingredientList = recipeParameters[2].split(", ");
+                for (String ingredient : ingredientList) {
+                    String[] ingredientQuantity = ingredient.split("x ");
+                    try {
+                        Item item = findItemByName(ingredientQuantity[1]);
+                        ingredients.put(item, Integer.valueOf(ingredientQuantity[0]));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                Map<Item, Integer> products = new HashMap<>();
+                String[] productList = recipeParameters[3].split(", ");
+                for (String product : productList) {
+                    String[] productQuantity = product.split("x ");
+                    try {
+                        Item item = findItemByName(productQuantity[1]);
+                        ingredients.put(item, Integer.valueOf(productQuantity[0]));
+                        products.put(item, Integer.valueOf(productQuantity[0]));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                Recipe recipe = new Recipe(recipeName, recipeDescription, ingredients, products);
+                recipes.add(recipe);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return recipes;
     }
 
@@ -59,6 +99,24 @@ public class GameController {
      * @return
      */
     public List<Item> loadItems() {
+        List<Item> items = new ArrayList<>();
+        Scanner sc;
+        try {
+            // pass the path to the file as a parameter
+            File file = new File(ITEM_FILE_LOCATION);
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                // Parse recipes line by line
+                String[] itemParameters = sc.nextLine().split(" \\| ");
+                String itemName = itemParameters[0];
+                String itemDescription = itemParameters[1];
+                int itemValue = Integer.parseInt(itemParameters[2]);
+                Item item = new Item(itemName, itemDescription, itemValue);
+                items.add(item);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return items;
     }
 
