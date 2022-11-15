a581e483f623f743e03d796fec505577786fd0c3
Juan Escalada <juanescalada175@gmail.com>
2022-11-14 16:45:58 -0800

    Add showShop method

--- src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameDriver.java
@@ -131,8 +131,99 @@ public class GameDriver extends Application {
         stage.setTitle("OppaiGames: The Awesome JavaFX Clicker Game");
         stage.show();
     }
-    private void showShop() {
-        // Should load and display the inventory panel with corresponding info
+    private void showShop(Stage stage, GameController game) {
+        Map<Item, Integer> itemsInStock = new HashMap<>();
+        itemsInStock.put(new Item("Pepe Plushy", "A sad frog.", 10), 5);
+        itemsInStock.put(new Item("Milk", "Yummy milk.", 50), 3);
+        itemsInStock.put(new Item("Gold Bar", "A shiny 24K gold bar.", 1000), 8);
+
+        Map<Item, Integer> inventory = new HashMap<>();
+        inventory.put(new Item("Item 1", "First item", 10), 5);
+        inventory.put(new Item("Item 2", "Second item", 15), 2);
+        inventory.put(new Item("Item 3", "Third item", 7), 3);
+        inventory.put(new Item("Item 4", "Fourth item", 20), 8);
+
+
+        BorderPane root = new BorderPane();
+        HBox center = new HBox();
+        VBox leftPanel = new VBox();
+        VBox rightPanel = new VBox();
+
+        StackPane top = new StackPane();
+        StackPane bottom = new StackPane();
+        Scene scene = new Scene(root, 800, 800);
+
+        Text panelTitle = new Text("Shop");
+        Text leftPanelTitle = new Text("For Sale:");
+        Text rightPanelTitle = new Text("Sell items:");
+        leftPanel.getChildren().add(leftPanelTitle);
+        rightPanel.getChildren().add(rightPanelTitle);
+        panelTitle.setScaleX(3);
+        panelTitle.setScaleY(3);
+        leftPanelTitle.setScaleX(1.5);
+        leftPanelTitle.setScaleY(1.5);
+        rightPanelTitle.setScaleX(1.5);
+        rightPanelTitle.setScaleY(1.5);
+
+        for (Item item : itemsInStock.keySet()) {
+            HBox box = new HBox();
+            Text itemName = new Text(item.getName());
+            Text itemDescription = new Text(item.getDescription());
+            Button buyButton = new Button("_Buy");
+            Text quantityInStock = new Text("In Stock: " + itemsInStock.get(item));
+
+            box.getChildren().addAll(itemName, itemDescription, quantityInStock, buyButton);
+            BackgroundFill background_fill = new BackgroundFill(Color.LIGHTGOLDENRODYELLOW,
+                    CornerRadii.EMPTY, Insets.EMPTY);
+            Background background = new Background(background_fill);
+            box.setBackground(background);
+            box.setSpacing(20);
+            box.setPadding(new Insets(10));
+            leftPanel.getChildren().add(box);
+        }
+
+        for (Item item : inventory.keySet()) {
+            HBox box = new HBox();
+            Text itemName = new Text(item.getName());
+            Text itemValue = new Text(item.getValue() + "G");
+            Text itemQuantity = new Text("Qty: " + inventory.get(item));
+            Button sellButton = new Button("_Sell");
+
+            box.getChildren().addAll(itemName, itemValue, itemQuantity, sellButton);
+            BackgroundFill background_fill = new BackgroundFill(Color.PINK,
+                    CornerRadii.EMPTY, Insets.EMPTY);
+            Background background = new Background(background_fill);
+            box.setBackground(background);
+            box.setSpacing(20);
+            box.setPadding(new Insets(10));
+            rightPanel.getChildren().add(box);
+        }
+
+        Button backButton = new Button("_Back");
+        backButton.setOnAction(event -> {
+            start(stage);
+        });
+        backButton.setMinSize(50,50);
+
+        leftPanel.setAlignment(Pos.CENTER);
+        rightPanel.setAlignment(Pos.CENTER);
+        center.setAlignment(Pos.CENTER);
+        center.setPadding(new Insets(10));
+
+        top.getChildren().addAll(panelTitle);
+        top.setPadding(new Insets(20));
+
+        bottom.getChildren().add(backButton);
+
+        center.getChildren().addAll(leftPanel, rightPanel);
+
+        root.setCenter(center);
+        root.setTop(top);
+        root.setBottom(bottom);
+
+        stage.setScene(scene);
+        stage.setTitle("OppaiGames: The Awesome JavaFX Clicker Game");
+        stage.show();
     }
     private void showCraft() {
         // Should load and display the inventory panel with corresponding info
