c854b5d510a22ff23fad9bfa1054bf5981467777
Juan Escalada <juanescalada175@gmail.com>
2022-11-14 16:45:36 -0800

    Add showMap method

--- src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/oppaigames/GameDriver.java
@@ -72,8 +72,64 @@ public class GameDriver extends Application {
     private void showInventory() {
         // Should load and display the inventory panel with corresponding info
     }
-    private void showMap() {
-        // Should load and display the inventory panel with corresponding info
+    private void showMap(Stage stage, GameController game) {
+        List<GatheringPoint> gatheringPoints = new ArrayList<>();
+        gatheringPoints.add(new GatheringPoint("Abandoned Mine", "An abandoned mine.", null, null));
+        gatheringPoints.add(new GatheringPoint("Enchanted Forest", "A beautiful forest.", null, null));
+        gatheringPoints.add(new GatheringPoint("Moo Moo Farm", "A farm with lots of cows.", null, null));
+
+        BorderPane root = new BorderPane();
+        TilePane center = new TilePane();
+        StackPane top = new StackPane();
+        StackPane bottom = new StackPane();
+        Scene scene = new Scene(root, 800, 800);
+        Text panelTitle = new Text();
+        panelTitle.setText("Map");
+        panelTitle.setScaleX(3);
+        panelTitle.setScaleY(3);
+
+        for (GatheringPoint point : gatheringPoints) {
+            VBox box = new VBox();
+            Text itemName = new Text(point.getName());
+            Text itemDescription = new Text("                     ");
+            Button gatherButton = new Button("_Gather");
+
+            gatherButton.setMinSize(100, 50);
+
+            box.getChildren().addAll(itemName, itemDescription, gatherButton);
+            box.setScaleX(1);
+            box.setScaleY(1);
+            BackgroundFill background_fill = new BackgroundFill(Color.PALEGREEN,
+                    CornerRadii.EMPTY, Insets.EMPTY);
+            Background background = new Background(background_fill);
+            box.setBackground(background);
+            box.setPadding(new Insets(10));
+            center.getChildren().add(box);
+        }
+
+        Button backButton = new Button("_Back");
+        backButton.setOnAction(event -> {
+            start(stage);
+        });
+        backButton.setMinSize(50,50);
+
+        center.setPadding(new Insets(20));
+        center.setScaleX(1.5);
+        center.setScaleY(1.5);
+        center.setAlignment(Pos.CENTER);
+
+        top.getChildren().addAll(panelTitle);
+        top.setPadding(new Insets(20));
+
+        bottom.getChildren().add(backButton);
+
+        root.setCenter(center);
+        root.setTop(top);
+        root.setBottom(bottom);
+
+        stage.setScene(scene);
+        stage.setTitle("OppaiGames: The Awesome JavaFX Clicker Game");
+        stage.show();
     }
     private void showShop() {
         // Should load and display the inventory panel with corresponding info
